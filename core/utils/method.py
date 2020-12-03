2fb7455351a3bd8760d10294aeaa6f5e993e79d2
zxx <xx.zong@idiaoyan.com>
2020-12-03 15:58:00 +0800

    Merge branch 'main' of D:\API with conflicts.

--- core/utils/method.py
+++ core/utils/method.py
@@ -6,6 +6,7 @@ import arrow
 from faker import Factory
 import os
 import time
+
 """
 创建测试数据方法
 """
@@ -36,11 +37,13 @@ def long_num(lenth=1):
     num = (''.join(random.choice(text) for i in range(lenth)))
     return int(num)
 
+
 # 随机取任意数量字符
 def Uppercase(number=1):
     text = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     text_new = (''.join(random.choice(text) for i in range(number)))
     return text_new
+
 
 # 随机取任意数量字符
 def lowercase(number=1):
@@ -48,12 +51,14 @@ def lowercase(number=1):
     text_new = (''.join(random.choice(text) for i in range(number)))
     return text_new
 
+
 # 随机取任意数量字符
 def chinese(number=1):
     text = '离离原上草，一岁一枯荣。野火烧不尽，春风吹又生。远芳侵古道，晴翠接荒城。又送王孙去，萋萋满别情。人间四月芳菲尽，山寺桃花始盛开。长恨春归无觅处，不知转入此中来。天长地久有时尽，此恨绵绵无绝期。在天愿作比翼鸟，在地愿为连理枝。' \
-	'别有幽愁暗恨生，此时无声胜有声。同是天涯沦落人，相逢何必曾相识。细草微风岸，危樯独夜舟。星垂平野阔，月涌大江流。名岂文章著，官应老病休。飘飘何所似？天地一沙鸥。'
+           '别有幽愁暗恨生，此时无声胜有声。同是天涯沦落人，相逢何必曾相识。细草微风岸，危樯独夜舟。星垂平野阔，月涌大江流。名岂文章著，官应老病休。飘飘何所似？天地一沙鸥。'
     text_new = (''.join(random.choice(text) for i in range(number)))
     return text_new
+
 
 # 随机取任意数量字符 （缺'_'）
 def half_angle(number=1):
@@ -61,7 +66,8 @@ def half_angle(number=1):
     text_new = (''.join(random.choice(text) for i in range(number)))
     return text_new
 
-#符号字符
+
+# 符号字符
 def full_symbol(number=1):
     text = r'`~！#￥%……&*（）——-+={【}】|、《，》。？、：；"'
     text_new = (''.join(random.choice(text) for i in range(number)))
@@ -91,8 +97,9 @@ def Arabic(number=1):
 
 # 除数字下划线字母以为的字符组合
 def numberTitle(chi=1, japen=1, fullSymbol=1, halfSy=1):
-    text = chinese(chi) + japanese(japen)  + full_symbol(fullSymbol)  +half_angle(halfSy)
+    text = chinese(chi) + japanese(japen) + full_symbol(fullSymbol) + half_angle(halfSy)
     return text
+
 
 # 把所有常用类型的字符拼接在了一起，默认都取一个字符
 def usu_text(lower=1, upper=1, number=1, half_angle_symbol=1):
@@ -107,16 +114,20 @@ def ob_text(chi=1, japen=1, arabic=1, fullSymbol=1, Emoji=1):
 
 
 # 把所有常用和不常用类型的字符拼接在了一起，默认都取一个字符
-def al_text(lower=1, upper=1, number=1, half_angle_symbol=1,chi=1, japen=1, arabic=1, fullSymbol=1, Emoji=1):
-    text = usu_text(lower=lower, upper=upper, number=number, half_angle_symbol=half_angle_symbol) + ob_text(chi=chi, japen=japen, arabic=arabic, fullSymbol=fullSymbol, Emoji=Emoji)
+def al_text(lower=1, upper=1, number=1, half_angle_symbol=1, chi=1, japen=1, arabic=1, fullSymbol=1, Emoji=1):
+    text = usu_text(lower=lower, upper=upper, number=number, half_angle_symbol=half_angle_symbol) + ob_text(chi=chi,
+                                                                                                            japen=japen,
+                                                                                                            arabic=arabic,
+                                                                                                            fullSymbol=fullSymbol,
+                                                                                                            Emoji=Emoji)
     return text
 
 
 # info 多少长度的文本  带空格
 def _info(num):
     info = ''
-    for i in range(int(num/5)):
-       info = info + '%s ' % (f.pystr(min_chars=4, max_chars=4))
+    for i in range(int(num / 5)):
+        info = info + '%s ' % (f.pystr(min_chars=4, max_chars=4))
     return info
 
 
@@ -130,7 +141,7 @@ def now_time():
 # get now timestamp
 def now_timestamp():
     curTime = arrow.now()
-    t = curTime.timestamp*1000
+    t = curTime.timestamp * 1000
     return t
 
 
@@ -154,22 +165,23 @@ def getFormatTime(type='days', num=0, format='YYYY-MM-DD HH:mm:ss', tzinfo=None)
 
 
 # get 当前天的间隔time
-def getTimestamp(type='days', num=0, hour=00, minute=00, second=00 ):
-    if type =='days':
-        return arrow.now().shift(days=num).replace(hour=hour,minute=minute,second=second).timestamp*1000
-    if type =='weeks':
-        return arrow.now().shift(weeks=num).replace(hour=hour,minute=minute,second=second).timestamp*1000
-    if type =='months':
-        return arrow.now().shift(months=num).replace(hour=hour,minute=minute,second=second).timestamp*1000
-    if type =='years':
-        return arrow.now().shift(years=num).replace(hour=hour,minute=minute,second=second).timestamp*1000
+def getTimestamp(type='days', num=0, hour=00, minute=00, second=00):
+    if type == 'days':
+        return arrow.now().shift(days=num).replace(hour=hour, minute=minute, second=second).timestamp * 1000
+    if type == 'weeks':
+        return arrow.now().shift(weeks=num).replace(hour=hour, minute=minute, second=second).timestamp * 1000
+    if type == 'months':
+        return arrow.now().shift(months=num).replace(hour=hour, minute=minute, second=second).timestamp * 1000
+    if type == 'years':
+        return arrow.now().shift(years=num).replace(hour=hour, minute=minute, second=second).timestamp * 1000
     else:
         print('no value,please check void')
 
 
 # 自定义时间
 def getAnyTimestamp(year, month, day, hour=00, minute=00, second=00, tzinfo='+08.00'):
-    return arrow.Arrow(year=year, month=month, day=day, hour=hour, minute=minute, second=second, tzinfo=tzinfo).timestamp*1000
+    return arrow.Arrow(year=year, month=month, day=day, hour=hour, minute=minute, second=second,
+                       tzinfo=tzinfo).timestamp * 1000
 
 
 # email
@@ -188,7 +200,7 @@ def phone():
     return f.phone_number()
 
 
-#Chrome user-agent info
+# Chrome user-agent info
 def ChromeUserAgent():
     return f.chrome()
 
@@ -217,9 +229,9 @@ def getValue(dir):
 
 
 # 返回值为set 和 None
-def checkKeys(pre,dir):
+def checkKeys(pre, dir):
     res = set(dir.keys())
-    if pre<=res:
+    if pre <= res:
         pass
     else:
         dif = pre - res
@@ -227,7 +239,7 @@ def checkKeys(pre,dir):
 
 
 def _string(n1, n2):
-    str = f.pystr(min_chars=n1,max_chars=n2)
+    str = f.pystr(min_chars=n1, max_chars=n2)
     return str
 
 
@@ -242,13 +254,11 @@ def user_agent():
 
 
 # 返回值为set 和 None
-def checkValue(dir,key,value):
+def checkValue(dir, key, value):
     if dir[key] != value:
         return key
     else:
         pass
-
-
 
 
 # 生成时间戳
@@ -261,11 +271,11 @@ def timestr():
 
 
 # 固定值拆分
-def fixSum(len,sum):
+def fixSum(len, sum):
     l = []
     for i in range(len):
-        if i != len-1:
-            i = random.randrange(0,sum)
+        if i != len - 1:
+            i = random.randrange(0, sum)
             sum = sum - i
             l.append(i)
         else:
@@ -278,17 +288,18 @@ def fixSum(len,sum):
 def fixchoose(num, l_choose):
     l = []
     for i in range(num):
-        if i != num-1:
+        if i != num - 1:
             l1 = random.sample(l_choose, k=random.randrange(0, num + 1))
             l.append(l1)
             l_choose = [i for i in l_choose if i not in l1]
     l.append(l_choose)
     return l
 
+
 # 时分计算
 def hm_time():
-    hour = random.randrange(0,25)*3600
-    minu = random.randrange(0,60)*60
+    hour = random.randrange(0, 25) * 3600
+    minu = random.randrange(0, 60) * 60
     tt = hour + minu
     return tt
 
@@ -299,7 +310,6 @@ def strTimeProp(start='1971-01-01', end='2050-12-31', prop=random.random(), frmt
     etime = time.mktime(time.strptime(end, frmt))
     ptime = stime + prop * (etime - stime)
     return int(ptime)
-
 
 
 # 读取excel问卷
@@ -313,4 +323,4 @@ def readExcelFile(filename='protest-原话记录.xlsx'):
 
 
 if __name__ == '__main__':
-   print(getTimestamp())
+    print(getTimestamp())

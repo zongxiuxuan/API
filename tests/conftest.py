d92baed258b26c0c5db14bbd0c30388b3655823a
zxx <87859738@qq.com>
2021-01-18 22:49:52 +0800

    优化发送测试报告通知

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,52 +13,53 @@ log.Logging()
 
 
 def pytest_runtest_call(__multicall__):
-    try:
-        __multicall__.execute()
-    except KeyboardInterrupt:
-        raise
-    except:
-        logging.exception('pytest_runtest_call caught exception:')
-        raise
+	try:
+		__multicall__.execute()
+	except KeyboardInterrupt:
+		raise
+	except:
+		logging.exception('pytest_runtest_call caught exception:')
+		raise
 
 
 def pytest_terminal_summary(terminalreporter, exitstatus, config):
-    """
-    收集测试结果 发送测试报告
-    :param terminalreporter:
-    :param exitstatus:
-    :param config:
-    :return:
-    """
-    total = terminalreporter._numcollected
-    passed = len([i for i in terminalreporter.stats.get('passed', []) if i.when != 'teardown'])
-    failed = len([i for i in terminalreporter.stats.get('failed', []) if i.when != 'teardown'])
-    error = len([i for i in terminalreporter.stats.get('error', []) if i.when != 'teardown'])
-    skipped = len([i for i in terminalreporter.stats.get('skipped', []) if i.when != 'teardown'])
-    success_rate = len(terminalreporter.stats.get('passed', [])) / terminalreporter._numcollected * 100
-    duration = time.time() - terminalreporter._sessionstarttime
-    data = f'total: {total}, passed: {passed}, failed: {failed}, error: {error}, skipped: {skipped}, success_rate: {success_rate}, times: {duration}'
-    logging.info(data)
-    flag = _config['email']['flag']
-    stmp_address = _config['email']['stmp_address']
-    stmp_password = _config['email']['stmp_password']
-    stmp_server = _config['email']['stmp_server']
-    stmp_port = _config['email']['stmp_port']
-    send_to = _config['email']['send_to']
-    text = f'{data}/n点击<a href="http://localhost:8883/">查看详情</a>'
-    send_email.sendReport(flag, send_to, stmp_address, stmp_password, stmp_server, stmp_port, text=text)
+	"""
+	收集测试结果 发送测试报告
+	:param terminalreporter:
+	:param exitstatus:
+	:param config:
+	:return:
+	"""
+	total = terminalreporter._numcollected
+	passed = len([i for i in terminalreporter.stats.get('passed', []) if i.when != 'teardown'])
+	failed = len([i for i in terminalreporter.stats.get('failed', []) if i.when != 'teardown'])
+	error = len([i for i in terminalreporter.stats.get('error', []) if i.when != 'teardown'])
+	skipped = len([i for i in terminalreporter.stats.get('skipped', []) if i.when != 'teardown'])
+	success_rate = len(terminalreporter.stats.get('passed', [])) / terminalreporter._numcollected * 100
+	duration = time.time() - terminalreporter._sessionstarttime
+	data = f'total: {total}, passed: {passed}, failed: {failed}, error: {error}, skipped: {skipped}, success_rate: {success_rate}%, times: {duration:.2f}'
+	logging.info(data)
+	flag = _config['email']['flag']
+	stmp_address = _config['email']['stmp_address']
+	stmp_password = _config['email']['stmp_password']
+	stmp_server = _config['email']['stmp_server']
+	stmp_port = _config['email']['stmp_port']
+	send_to = _config['email']['send_to']
+	mail_msg = f'<p>API测试运行完成</p><p>执行情况: {data}</p><p>点击 <a href="http://localhost:8883/">查看详情</a></p>'
+	send_email.sendReport(flag, send_to, stmp_address, stmp_password, stmp_server, stmp_port, file=None, text=mail_msg,
+						  textType='html')
 
 
 @pytest.fixture(scope='session')
 def set_headers(user='admin_user'):
-    url = f'{_config["env"]}/api/authorize/v2/token/'
-    user = api_yaml.get_user(user)
-    payload = {
-        'is_home_page': False,
-        'org_code': user['org_code'],
-        'user_name': user['user_name'],
-        'password': user['password']
-    }
-    result = request.post(url, payload=payload, env=False)
-    token = f"Bearer {result.json()['data']['token']}"
-    return token
+	url = f'{_config["env"]}/api/authorize/v2/token/'
+	user = api_yaml.get_user(user)
+	payload = {
+		'is_home_page': False,
+		'org_code': user['org_code'],
+		'user_name': user['user_name'],
+		'password': user['password']
+	}
+	result = request.post(url, payload=payload, env=False)
+	token = f"Bearer {result.json()['data']['token']}"
+	return token
--- utils/send_email.py
+++ utils/send_email.py
@@ -2,60 +2,53 @@
 import smtplib
 from email.mime.text import MIMEText
 from email import encoders
+from email.header import Header
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from utils import getDir
-import time
 import os
 
 
 # 使用email模块
-def sentMail(to_addr, from_addr, passwd, smtp_server, smtp_port, file=None, text=None):
-    msg = MIMEMultipart()
-    # msg.attach(body)
-    msg['From'] = from_addr
+def sentMail(receivers, stmp_address, smtp_password, smtp_server, smtp_port, file=None, text=None, textType='plain'):
+	msg = MIMEMultipart()
+	sender = stmp_address
+	receivers = receivers
+	# 创建一个带附件的实例
+	msg['From'] = Header('tester', 'utf-8')
+	msg['To'] = ','.join(receivers)
+	msg['Subject'] = 'Auto Testing Report'
+	# 添加正文和附件
+	if file:
+		part = MIMEBase('application', 'octet-stream')
+		part.set_payload(open(file, 'rb').read())
+		encoders.encode_base64(part)
+		part.add_header('Content-Disposition', 'attachment; filename="test_report.html"')
+		msg.attach(part)
+	if text:
+		msg.attach(MIMEText(text, textType, 'utf-8'))
+	# 发送邮件
+	try:
+		server = smtplib.SMTP(smtp_server, smtp_port)
+		server.set_debuglevel(1)
+		server.login(stmp_address, smtp_password)
+		server.sendmail(sender, receivers, msg.as_string())
+		server.quit()
+	except smtplib.SMTPException:
+		print("Error: 无法发送邮件")
+	print(u'Mail sent successfully')
 
-    # 多个邮件接收者需要修改to_addr
-    msg['To'] = ','.join(to_addr)
-    msg['Subject'] = 'API Auto Testing Report'
-    msg['date'] = time.strftime('%a,%d %b %Y %H:%M:%S %z')
-    if not file:
-        part = MIMEBase('application', 'octet-stream')
-        part.set_payload(open(file, 'rb').read())
-        encoders.encode_base64(part)
-        part.add_header('Content-Disposition', 'attachment; filename="test_report.html"')
-        msg.attach(part)
-    if not text:
-        msg.attach(MIMEText(text, 'plain', 'utf-8'))
-        # body = MIMEText(text, _charset='utf-8')
-        # msg.attach(body)
-        # msg['Body'] = text
-    server = smtplib.SMTP(smtp_server, smtp_port)
-    server.set_debuglevel(1)
-    server.login(from_addr, passwd)
-    # for发给多个接收者
-    # for to_addr in addrs:
-    server.sendmail(from_addr, to_addr, msg.as_string())
-    server.quit()
-    print(u'Mail sent successfully')
 
-
-def sendReport(flag, to_addr, from_addr, passwd, smtp_server, smtp_port, file=None, text=None):
-    if flag == 'true':
-        if not file:
-            file = os.path.join(getDir.proDir, "report/report.html")
-            # lists = os.listdir(resultPath)
-            # # 找到最新的测试报告
-            # lists.sort(key=lambda fn: os.path.getmtime(resultPath + fn) if not os.path.isdir(resultPath + fn) else 0)
-            # # 找到最新的文件
-            # newFile = os.path.join(resultPath, lists[-1])
-            # # print(newFile)
-            # # 调用发邮件模块
-            sentMail(to_addr, from_addr, passwd, smtp_server, smtp_port, file)
-        elif not text:
-            text = text
-            sentMail(to_addr, from_addr, passwd, smtp_server, smtp_port, text)
-        else:
-            pass
-    else:
-        print('send email functional is off')
+def sendReport(flag, receivers, stmp_address, smtp_password, smtp_server, smtp_port, file=None, text=None,
+			   textType='plain'):
+	if flag == 'true':
+		sentMail(receivers, stmp_address, smtp_password, smtp_server, smtp_port, file, text, textType)
+	# lists = os.listdir(resultPath)
+	# # 找到最新的测试报告
+	# lists.sort(key=lambda fn: os.path.getmtime(resultPath + fn) if not os.path.isdir(resultPath + fn) else 0)
+	# # 找到最新的文件
+	# newFile = os.path.join(resultPath, lists[-1])
+	# # print(newFile)
+	# # 调用发邮件模块
+	else:
+		print('send email functional is off')
